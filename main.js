4995d96a14f5d0a7a091ac14feef1feb37788504
Martina Kafadar <martina.kafa7@gmail.com>
2023-02-19 16:35:06 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const middleDiv = document.getElementById("middle-div");
+const paragraphs = document.getElementsByTagName("p");
+let currentParagraphIndex = 0;
+
+setInterval(() => {
+  // hide the current paragraph
+  paragraphs[currentParagraphIndex].style.display = "none";
+
+  // increment the current paragraph index and loop back to the first paragraph if necessary
+  currentParagraphIndex = (currentParagraphIndex + 1) % paragraphs.length;
+
+  // show the next paragraph
+  paragraphs[currentParagraphIndex].style.display = "block";
+}, 2000);
+
+// initially hide all paragraphs except the first one
+for (let i = 1; i < paragraphs.length; i++) {
+  paragraphs[i].style.display = "none";
+}
